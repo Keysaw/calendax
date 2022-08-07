9451feee2550e1ac1ae9b685b4e04eb2a0145b40
Keysaw <22835200+Keysaw@users.noreply.github.com>
2022-08-07 12:17:07 +0200

    Added @calendaxScripts directive

--- src/CalendaxServiceProvider.php
+++ src/CalendaxServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Brickx\Calendax;
 
 use Brickx\Calendax\Commands\CalendaxCommand;
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -14,5 +15,42 @@ class CalendaxServiceProvider extends PackageServiceProvider
 			->hasConfigFile()
 			->hasViews()
 			->hasCommand(CalendaxCommand::class);
+	}
+
+	public function bootingPackage()
+	{
+		Blade::directive('calendaxScripts', function () {
+			return <<<'HTML'
+            <script>
+                function onCalendaxEventDragStart(event, eventId) {
+                    event.dataTransfer.setData('id', eventId);
+                }
+                function onCalendaxEventDragEnter(event, componentId, dateString, dragAndDropClasses) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    let element = document.getElementById(`${componentId}-${dateString}`);
+                    element.className = element.className + ` ${dragAndDropClasses} `;
+                }
+                function onCalendaxEventDragLeave(event, componentId, dateString, dragAndDropClasses) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    let element = document.getElementById(`${componentId}-${dateString}`);
+                    element.className = element.className.replace(dragAndDropClasses, '');
+                }
+                function onCalendaxEventDragOver(event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                }
+                function onCalendaxEventDrop(event, componentId, dateString, year, month, day, dragAndDropClasses) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    let element = document.getElementById(`${componentId}-${dateString}`);
+                    element.className = element.className.replace(dragAndDropClasses, '');
+                    const eventId = event.dataTransfer.getData('id');
+                    window.Livewire.find(componentId).call('onEventDropped', eventId, year, month, day);
+                }
+            </script>
+HTML;
+		});
 	}
 }
