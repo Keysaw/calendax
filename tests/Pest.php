242630852503ebebb1929caef4b6a10fad8e40bf
Keysaw <22835200+Keysaw@users.noreply.github.com>
2022-08-10 13:18:21 +0200

    Updated Pest setup

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,9 +1,10 @@
 <?php
 
 use Brickx\Calendax\Tests\TestCase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 use Illuminate\Support\Carbon;
 
-uses(TestCase::class)->in(__DIR__);
+uses(TestCase::class, InteractsWithViews::class)->in('Unit', 'Feature');
 
 expect()->extend('toBeWithin', function (Carbon $start, Carbon $end) {
 	return $this->get('startsAt')->toEqual($start) && $this->get('endsAt')->toEqual($end);
